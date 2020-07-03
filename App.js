1067502543b1f5009b87c580d90af3045d29714d
Alireza B <Alireza@simor.org>
2020-07-02 22:46:49 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React , {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class App extends Component {
+  render (){
+    return (
+      <View style={styles.container}>
+        <Text>Hello App</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+}
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
